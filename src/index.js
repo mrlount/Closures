05034a1771b2367b61212346b1c73add5ded0641
Chris Lount <chris.lount@warerebel.com>
2016-06-05 13:07:16 +0100

    Use routes

--- src/index.js
+++ src/index.js
@@ -3,17 +3,15 @@
 var express = require('express');
 var helmet = require('helmet');
 var compression = require('compression');
+var router = require('./routes.js');
 var app = express();
 
 app.set('view engine', 'pug');
 app.use(helmet());
+app.use(compression());
 app.use('/resources', express.static('node_modules'));
 app.use('/static', express.static('static'));
-app.use(compression());
-
-app.get('/', function (req, res) {
-  res.render('index');
-});
+app.use('/', router);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
