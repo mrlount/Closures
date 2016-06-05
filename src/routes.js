8ad6ebf5f82cf87dfcc4ccab26d039927367eade
Chris Lount <chris.lount@warerebel.com>
2016-06-05 13:07:30 +0100

    get events

--- src/routes.js
+++ src/routes.js
@@ -1,2 +1,24 @@
 "use strict";
 
+var express = require('express');
+var router = express.Router();
+var eventsServiceModule = require('./eventService.js');
+var querystring = require("querystring");
+var url = require("url");
+
+var eventService = new eventsServiceModule();
+eventService.updateEvents();
+
+router.get('/', function(req, res) {
+  res.render('index');;
+});
+
+router.get("/events", function(req, res){
+    var query = querystring.parse(url.parse(req.url).query);
+    eventService.getEvents( query, function(error, results){
+        res.json(results);
+    });    
+});
+
+module.exports = router;
+
