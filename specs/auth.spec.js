60e9ca7f8e5c6ac63195be15c9c082b80fc5e6fa
Chris Lount <chris.lount@warerebel.com>
2016-06-07 19:50:51 +0100

    Auth module

---
+++ specs/auth.spec.js
@@ -0,0 +1,17 @@
+var fs = require("fs");
+var authModule = require("../src/auth.js");
+
+var pinArray = [12, 16, 18, 19, 20];
+var json = JSON.stringify(pinArray);
+fs.writeFileSync("pins.json", json, "utf8");
+
+var auth = new authModule();
+
+describe("It checks different pins to see if they are valid", function(){
+    it("Checks a correct pin", function(){
+        expect(auth.checkPin(12)).toBe(true);
+    });
+    it("Checks an incorrect pin", function(){
+        expect(auth.checkPin(100)).toBe(false);
+    });
+});
---
+++ src/auth.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var fs = require("fs");
+
+function auth(){
+    this.pinArray = [];
+    var me = this;
+    var result = fs.readFileSync("pins.json","utf8");
+    var input = JSON.parse(result);
+    input.forEach(function(pin){
+        me.pinArray.push(pin);
+    });
+}
+
+auth.prototype.checkPin = function(pin)
+{
+    var testPin;
+    testPin = typeof(pin) === "string" ? pin = parseInt(pin) : pin;
+
+    if(this.pinArray.indexOf(pin) > -1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+module.exports = auth;
